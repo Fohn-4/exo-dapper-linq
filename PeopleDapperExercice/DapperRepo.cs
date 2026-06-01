99e72b380a3e773decea38862164bd132a75a0cf
Fohn-4 <renardstaifane@gmail.com>
2026-06-01 19:45:25 +0200

    CRUD is implemented in DapperRepo.cs

--- PeopleDapperExercice/DapperRepo.cs
+++ PeopleDapperExercice/DapperRepo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.VisualBasic;
 using MySql.Data.MySqlClient;
 
 namespace PeopleDapperExercice
@@ -22,6 +23,52 @@ namespace PeopleDapperExercice
                 connection.Open();
                 string sql = "INSERT INTO People (Name, Age) VALUES (@Name, @Age)";
                 connection.Execute(sql, person);
+            }
+        }
+
+        public Person GetbyId(int id)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                Person person = connection.QuerySingleOrDefault<Person>(sql, new { Id = id });
+
+                if (person == null)
+                {
+                    throw new Exception($"Aucune personne trouvée avec l'id {id}");
+                }
+                
+                return person;
+            }
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM People ORDER BY Name";
+                return connection.Query<Person>(sql);
+            }
+        
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sql = "UPDATE People SET Name = @Name, Age = @Age Where Id = @Id";
+                connection.Execute(sql, person);
+            }
+        }
+
+        public void DeletePerson(int id)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sql = "DELETE FROM People WHERE Id = @Id";
             }
         }
     }
